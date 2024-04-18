7e57cc10edbd736241f688b1ce57757b4f386441
Rama Raghu <ramanadar003@gmail.com>
2024-04-18 11:39:09 +0530

    Home Page completed

--- app/courses/page.jsx
+++ app/courses/page.jsx
@@ -3,11 +3,15 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import { useState, useEffect } from 'react'
+import image2 from '../../public/assets/image2.jpeg'
+
+
 
 
 function Courses() {
   const [courses, setCourses] = useState([])
   const [isLoading, setLoading] = useState(true)
+
 
   useEffect(() => {
     fetch('/api/courses')
@@ -16,75 +20,115 @@ function Courses() {
         setCourses(data.courses)
         setLoading(false)
 
+
       })
   }, [])
   // console.log("THE DATA ISSSSs",JSON.stringify(courses));
 
-  return (
+
+
+
+
+
+
+
+  const data1 = [
+    {
+      "title": "Global Impact",
+      'Description': "Blockchain technology has the potential to address global challenges and allows individuals to contribute to social impact initiatives and explore the technologies.",
+      "subhead": [
+        "Financial inclusion.",
+        "identity management",
+        "Launch blockchain startups.",
+      ]
+    },
+    {
+      "title": "Emerging Technology",
+      'Description': "Blockchain is a rapidly evolving technology with potential applications across various industries.    ",
+      "subhead": [
+        "finance ",
+        "healthcare",
+        "supply chain management",
+
+
+      ]
+    },
+    {
+      "title": "Investment and Trading",
+      'Description': "Understanding blockchain technology can help investors make informed decisions and navigate the complexities of the cryptocurrency ecosystem.",
+      "subhead": [
+        "Investment strategies.",
+        "Trading techniques",
+        "cryptocurrency markets",
+      ]
+    },
+    {
+      "title": "Career Opportunities",
+      'Description': "Understanding blockchain technology can inspire and find entrepreneurial ventures.",
+      "subhead": [
+        "Blockchain-based business models.",
+        "Develop decentralized applications.",
+        "Launch blockchain startups.",
+        "Launch blockchain startups.",
+      ]
+    },
+  ]
+
+
+
+
+  // for course page
+  const blockchaincourse =  [
+    {
+      "id": 1,
+      "title": 'Immutability',
+      "image": "Img1",
+    },
+    {
+      "id": 2,
+      "title": 'Reduced Costs',
+      "image": "Img2",
+    },
+    {
+      "id": 3,
+      "title": 'Improved Efficiency',
+      "image": "Img3",
+    },
+   
+  ]
+
+
+  const course2=[
+    {
+      "id": 4,
+      "title": 'Traceability',
+      "image": "Img4",
+    },
+    {
+      "id": 5,
+      "title": 'Enhanced Security',
+      "image": "Img5",
+    },
+    {
+      "id": 6,
+      "title": 'Tansparency',
+      "image": "Img6",
+    },
+  ]
+
+
+
+
+  return (<>
     <div className=''>
       <div className='flex flex-col text-center p-4'>
         <h1 className='text-3xl font-bold'> Our Courses</h1>
         <p className='text-xl'>Unlock Your Potential with Our Online Programs</p>
       </div>
-      {/*  */}
-      
-         < div className='m-12'>
-
-          <div className='grid md:grid-col-3 sm:grid-cols-2 mt-12 gap-8'>
-            <div className=' flex items-center flex-col gap-4 bg-blue-100 p-8 rounded-md '>
-              <h1 className='font-bold text-2xl'>Emerging Technology</h1>
-              <p>Blockchain is a rapidly evolving technology with potential applications across various industries.</p>
-              <ul className='list-disc' >
-                <li>finance  </li>
-                <li>healthcare</li>
-                <li>supply chain management</li>
-              </ul>
-            </div>
-
-            {/*******************************************************************************/}
-            <div className=' flex items-center flex-col gap-4 bg-blue-100 p-8 rounded-md'>
-              <h1 className='font-bold text-2xl'>Career Opportunities</h1>
-              <p>Understanding blockchain technology can inspire and find entrepreneurial ventures.</p>
-              <ul className='list-disc'>
-                <li>Blockchain-based business models.</li>
-                <li>Develop decentralized applications.</li>
-                <li>Launch blockchain startups.</li>
-              </ul>
-            </div>
 
 
-            <div className=' flex items-center flex-col gap-4 bg-blue-100 p-8 rounded-md'>
-              <h1 className='font-bold text-2xl'>Investment and Trading</h1>
-              <p>Understanding blockchain technology can help investors make informed decisions and navigate
-                the complexities of the cryptocurrency ecosystem.</p>
-              <ul className='list-disc'>
-                <li>Investment strategies.</li>
-                <li>Trading techniques</li>
-                <li>cryptocurrency markets</li>
-              </ul>
-            </div>
-
-
-            <div className=' flex items-center flex-col gap-4 bg-blue-100 p-8 rounded-md'>
-              <h1 className='font-bold text-2xl'>Global Impact</h1>
-              <p>Blockchain technology has the potential to address global challenges
-                and allows individuals to contribute to social impact initiatives
-                and explore the technologies.</p>
-              <ul className='list-disc'>
-                <li>Financial inclusion.</li>
-                <li>identity management</li>
-                <li>Supply chain transparency.</li>
-
-
-              </ul>
-            </div>
-
-
-          </div>
-     
-          </div>
-      {/*  */}
       <div className='grid justify-between gap-10 px-15 py-4 md:grid-cols-2 lg:grid-cols-2'>
+
 
         {courses.map((c) => (
           <Link key={c.id} href={`/courses/${c.id}`} className='border m-auto rounded-lg transform transition duration-500 hover:scale-105 md:hover:scale-110'>
@@ -99,15 +143,69 @@ function Courses() {
                 <h1 className='pt-3 text-2xl font-semibold'>{c.name}</h1>
                 <p>62 modules</p>
 
+
               </div>
+
 
             </div>
           </Link>
         ))}
       </div>
-    </div>
+  
+
+    
+      <div className='text-3xl bg-[#15265D] p-5 font-bold justify-center text-center'>
+        <h2 className='text-white'>Building the Future<span className='text-[#FF8C19] px-2'>With Blockchain</span></h2>
+
+
+      </div>
+      <div className='bg-[#fff] md:p-10 flex sm:justify-between md:justify-between'>
+        <ul className='p-5 text-center'><li className='bg-[#15265D] p-2 mb-2 text-white'>Immutability
+           </li>
+          <li className='bg-[#15265D] p-2 mb-2 text-white'>Reduced Costs</li>
+          <li className='bg-[#15265D] p-2 mb-2 text-white'>Improved Efficiency</li>
+          </ul>
+         <Image src={image2} className='w-[200px] h-[200px] rounded-full'/>
+          <ul className='p-5 text-center '>
+          <li className='bg-[#15265D] p-2 mb-2 text-white'>Traceability</li>
+          <li className='bg-[#15265D] p-2 mb-2 text-white'>Enhanced Security</li>
+          <li className='bg-[#15265D] p-2 mb-2 text-white'>Tansparency</li>
+          </ul>
+      </div>
+
+
+      {/* ********************************************************************************************** */}
+      <div className='grid justify-between gap-10 px-15 py-4 md:grid-cols-2 lg:grid-cols-2'>
+
+
+        {data1.map((e) => (
+          <div key={e.id} className=' flex items-center flex-col gap-4 bg-blue-100 p-8 rounded-md '>
+            <h1 className='font-bold text-2xl'>{e.title}</h1>
+            <p>{e.Description}</p>
+            <ul className='list-disc'>
+              {e.subhead.map((e1, i) => (
+                <li key={i}>{e1}</li>))}
+            </ul>
+          </div>
+
+
+        ))}
+
+
+      </div>
+   
+
+
+    </div></>
+
+
   )
 }
 export default Courses
+
+
+
+
+
 
 
